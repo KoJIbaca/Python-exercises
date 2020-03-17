b6fce37e737d05806f83b7342cd2991c3a9475f7
KoJIbaca <62202187+KoJIbaca@users.noreply.github.com>
2020-03-17 20:54:35 +0300

    Add files via upload
    Исходный код определения "пифагоровой тройки" (Python)

---
+++ Пифагорова тройка.py
@@ -0,0 +1,13 @@
+while True:
+    n=input('Введите до какого числа искать:\n')
+    try:
+        n=int(n)
+    except:
+        print('Нельзя вводить выражения или буквы\n')
+    else:
+        break
+for c in range (1,n+1, 1):
+    for a in range (1,c):
+        for b in range (1,a):
+            if c**0.5 == (a**2+b**2)**0.5:
+                print (a,b,c)
